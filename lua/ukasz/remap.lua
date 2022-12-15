1caf28c2ff5dc1a8458591c792760c42f990e402
Lukasz Cholewka <ukasz.cholewka@gmail.com>
2022-12-15 09:39:31 +0100

    treesitter and errors

--- lua/ukasz/remap.lua
+++ lua/ukasz/remap.lua
@@ -2,7 +2,8 @@ local nnoremap = require("ukasz.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
-nnoremap("<leader>f", "<cmd>GFiles<CR>")
+nnoremap("<leader>ff", "<cmd>GFiles<CR>")
+nnoremap("<leader>fr", "<cmd>Rg<CR>")
 
 nnoremap("<leader>w", "<C-w>")
 nnoremap("<leader>wv", "<C-w><C-v><C-w>l")
@@ -17,3 +18,11 @@ nnoremap("<C-j>", "<cmd>:lua require(\"harpoon.ui\").nav_next()<CR>")
 nnoremap("<C-k>", "<cmd>:lua require(\"harpoon.ui\").nav_prev()<CR>")
 
 nnoremap("<leader>sv", "<cmd>call CocActionAsync('doHover'),<CR>")
+
+nnoremap("gb", "<cmd>:e#<CR>")
+nnoremap("gv", "<cmd>:e#2<CR>")
+
+nnoremap("<C-u>", "<C-u>zz")
+nnoremap("<C-d>", "<C-d>zz")
+
+nnoremap("<C-o>", "<C-o>zz")
--- lua/ukasz/set.lua
+++ lua/ukasz/set.lua
@@ -35,16 +35,25 @@ end
 keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
 
 vim.api.nvim_create_augroup("CocGroup", {})
+
 vim.api.nvim_create_autocmd("CursorHold", {
     group = "CocGroup",
     command = "silent call CocActionAsync('highlight')",
     desc = "Highlight symbol under cursor on CursorHold"
 })
 
-vim.api.nvim_create_autocmd("CursorHold", {
-    command = "silent call CocActionAsync('doHover')",
-    desc = "Highlight symbol under cursor on CursorHold"
+vim.api.nvim_create_autocmd("FileType", {
+    group = "CocGroup",
+    pattern = "typescript,json",
+    command = "setl formatexpr=CocAction('formatSelected')",
+    desc = "Setup formatexpr specified filetype(s)."
 })
+
+-- vim.api.nvim_create_autocmd("CursorHold", {
+--     group = "CocGroup",
+--     command = "lua _G.show_docs()",
+--     desc = "Highlight symbol under cursor on CursorHold"
+-- })
 
 keyset("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
 
@@ -52,3 +61,63 @@ keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
 keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
 keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
 keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
+
+local opts = {silent = true, nowait = true}
+keyset("x", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
+keyset("n", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
+
+
+-- Add `:Format` command to format current buffer.
+vim.api.nvim_create_user_command("Format", "call CocAction('format')", {})
+
+-- " Add `:Fold` command to fold current buffer.
+vim.api.nvim_create_user_command("Fold", "call CocAction('fold', <f-args>)", {nargs = '?'})
+
+-- Add `:OR` command for organize imports of the current buffer.
+vim.api.nvim_create_user_command("OR", "call CocActionAsync('runCommand', 'editor.action.organizeImport')", {})
+
+
+
+-- tree sitter: 
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "typescript", "lua", "rust", "javascript" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "c" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
